68cd0eef6848a56dda4521ca719b67441d363c52
Lucy Scanlon <lucyscanlon@Lucys-iMac.local>
2019-11-25 20:38:23 +0000

    FINISHED about to add required plugins

--- functions.php
+++ functions.php
@@ -29,6 +29,3 @@ require get_template_directory() . '/inc/widgets/socialmedia-widget.php';
 require get_template_directory() . '/inc/widgets/popularposts-widget.php';
 require get_template_directory() . '/inc/widgets/linktopage-widget.php';
 require get_template_directory() . '/inc/widgets/travelstats-widget.php';
-
-
-/* required plugins */
